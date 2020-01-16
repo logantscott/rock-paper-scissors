2e6d705505d29645c6d26b742fbf1a71d0cba304
Logan Scott <logantscott@gmail.com>
2020-01-15 17:48:47 -0800

    add script functions for throw and compare

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-import {getRandomThrow, checkResult} from './r-p-screen.js';
+import { getRandomThrow, checkResult } from './r-p-s.js';
+
+// const rockSelect = document.getElementById('rock');
+// const paperSelect = document.getElementById('rock');
+// const scissorsSelect = document.getElementById('rock');
+const submit = document.getElementById('rpssubmit');
+
+submit.addEventListener('click', () => {
+    const selection = document.getElementsByName('rpsinput');
+    for (let i = 0; i < selection.length; i++) {
+        if (selection[i].checked) {
+            checkResult(selection[i].value, getRandomThrow());
+        }
+    }
+    
+});
--- src/r-p-s.js
+++ src/r-p-s.js
@@ -1,7 +1,19 @@
 export function getRandomThrow() {
-
+    const computerThrow = Math.ceil(Math.random() * 3);
+    if (computerThrow === 1) return 'rock';
+    else if (computerThrow === 2) return 'paper';
+    else if (computerThrow === 3) return 'scissors';
 }
 
-export function checkResult() {
-    
+export function checkResult(userInput, cpuInput) {
+    const compare = ['rock', 'paper', 'scissors'].indexOf(cpuInput);
+
+    if (userInput === 'rock') {
+        return [0, -1, 1][compare];
+    } else if (userInput === 'paper') {
+        return [1, 0, -1][compare];
+    } else if (userInput === 'scissors') {
+        return [-1, 1, 0][compare];
+    }
 }
+
